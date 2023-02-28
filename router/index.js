6d159863fbf55111449571fb3d672659db297ac2
amirhossein <amirhosseinghodratnema@gmail.com>
2023-02-28 14:13:51 +0330

    Partioals file

--- router/index.js
+++ router/index.js
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     try {
-        return res.render('index.ejs');
+        const users = [
+            {id : 1, name : 'Amirhossein', lastName : 'Ghodratnema', age : 24},
+            {id : 2, name : 'Milad', lastName : 'Nemayan', age : 25},
+            {id : 3, name : 'Abolfazl', lastName : 'Seriyani', age : 18},
+        ]
+        return res.render('index', { users });
         // return res.status(200).json('Main route is ok...');  
     } catch (error) {
         next(error);
